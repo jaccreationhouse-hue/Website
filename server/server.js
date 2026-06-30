8bfc04d934bec805eadd423a483495c8dbd59368
Developer <dev@jaccreationhouse.com>
2026-06-30 18:46:54 +0530

    Fix dotenv path for deployment

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import publicRoutes from './routes/publicRoutes.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, '../.env') });
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
